f2a94735a6507c78d3d7ea25750644e20515579e
dthigpen <davidthigs@gmail.com>
2018-10-11 23:02:30 -0500

    Add script to control screen ui

---
+++ Equipment-Maintenance-in-VR/Assets/ScreenUIController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenUIController : MonoBehaviour {
+
+    private Canvas screenCanvas;
+    private Text topText;
+
+	// Use this for initialization
+	void Start () {
+        screenCanvas = GameObject.Find("ScreenCanvas").GetComponent<Canvas>();
+        topText = GameObject.Find("TopText").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetTopText(string text)
+    {
+        topText.text = text;
+    }
+
+    public void ClearTopText()
+    {
+        topText.text = "";
+    }
+
+    public void ShowTopText()
+    {
+        topText.gameObject.SetActive(false);
+    }
+
+    public void HideTopText()
+    {
+        topText.gameObject.SetActive(true);
+    }
+
+}
