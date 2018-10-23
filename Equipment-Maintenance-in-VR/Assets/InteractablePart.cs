2d08f851b0b992cfa3bc64b7ae28cb0d1b1c7847
dthigpen <davidthigs@gmail.com>
2018-10-22 21:59:43 -0500

    Turn of gravity when placed correctly

--- Equipment-Maintenance-in-VR/Assets/InteractablePart.cs
+++ Equipment-Maintenance-in-VR/Assets/InteractablePart.cs
@@ -11,18 +11,22 @@ public class InteractablePart : MonoBehaviour {
     private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
     private Interactable interactable;
 
-    private Transform transform;
+
     public Transform endPointTransform;
    
     public float acceptableDegreesFromEndPoint = 10f;
     public float acceptableMetersFromEndPoint = 0.1f;
     public bool snapAndDetach = true;
     public UnityEvent onAcceptablePlacement;
+
+    private Transform transform;
+    private Rigidbody rigidbody;
     private bool wasAcceptable = false;
     // Use this for initialization
     void Start () {
-        interactable = this.GetComponent<Interactable>();
-        transform = gameObject.transform;
+        interactable = GetComponent<Interactable>();
+        transform = GetComponent<Transform>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnAcceptablePlacement()
@@ -88,21 +92,30 @@ public class InteractablePart : MonoBehaviour {
             hand.HoverLock(interactable);
             // Attach this object to the hand
             hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+
+            // If it was picked up from the end point location apply gravity and kinematic again
+            if(transform.position == endPointTransform.position)
+            {
+                rigidbody.useGravity = true;
+                rigidbody.isKinematic = true;
+            }
         }
         else if (isGrabEnding)
         {
             // Detach this object from the hand
             hand.DetachObject(gameObject);
-
             // Call this to undo HoverLock
             hand.HoverUnlock(interactable);
-
             // For snapping
             if (IsWithinRangeOfCenter(endPointTransform, acceptableMetersFromEndPoint)
                 && IsWithinRangeOfRotation(gameObject.transform.rotation, endPointTransform.rotation, acceptableDegreesFromEndPoint))
             {
                 OnAcceptablePlacement();
                 wasAcceptable = true;
+                // stay in place when placed correctly
+                rigidbody.useGravity = false;
+                rigidbody.isKinematic = true;
+
                 // Move to End point transform
                 transform.position = endPointTransform.position;
                 transform.rotation = endPointTransform.rotation;
@@ -128,14 +141,11 @@ public class InteractablePart : MonoBehaviour {
                     {
                         // Detach this object from the hand
                         hand.DetachObject(gameObject);
-
                         // Call this to undo HoverLock
                         hand.HoverUnlock(interactable);
-                           
                         // Move to End point transform
                         transform.position = endPointTransform.position;
                         transform.rotation = endPointTransform.rotation;
-
                         endPointTransform.gameObject.SetActive(false);
                     }
                 }
