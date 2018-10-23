32ffe5ba2eb55849c46f73459d00bb0c0c372643
dthigpen <davidthigs@gmail.com>
2018-10-23 10:37:51 -0500

    Add PBR Worskshop assets
    buckle down, its a big one

---
+++ Equipment-Maintenance-in-VR/Assets/TirgamesAssets/PBRWorkShop/Models/PBRGarage/Scripts/GarageDoor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GarageDoor : MonoBehaviour {
+	public Transform	MessageUI;
+
+	Animation doorAnim;
+	bool opened=false; 
+	bool allowOpen;
+
+	// Use this for initialization
+	void Start () {
+		doorAnim=GetComponent<Animation>();
+		MessageUI.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (allowOpen && !opened) {
+			if (Input.GetKeyDown(KeyCode.E)) {
+				opened=true;
+				MessageUI.gameObject.SetActive(false);
+				doorAnim.Play("Open");
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		allowOpen=true;
+		if (!opened) {
+			MessageUI.gameObject.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		allowOpen=false;
+		if (!opened) {
+			MessageUI.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Equipment-Maintenance-in-VR/Assets/TirgamesAssets/PBRWorkShop/ReadMePBRWorkshop.txt
@@ -0,0 +1,15 @@
+Package requaire import Standart Assets and Effects
+For correct PBR visual effect asset uses HDR, and Linear Space:
+1. Set In Player settings->Other Settings ColorSpace to Linear
+2. Disable Project Settings -> Quality-> AntiAliasing
+
+Version history:
+1.0 - First release
+1.1 - Added animated doors
+1.2 - Added blueprints and table lamp
+1.3 - Added gas ballones and exterior fence
+
+
+email:		tirgames.assets@gmail.com
+Facebook:	https://www.facebook.com/tirgamesassets
+Twitter:	https://twitter.com/TirgamesAssets
