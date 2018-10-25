ec27a1bc3b0b9363459ce469462527f5e78f35fd
SharonLougheed <sharon.lougheed@gmail.com>
2018-10-24 19:11:15 -0500

    Testing 3D Text

--- Equipment-Maintenance-in-VR/Assets/ScreenUIController.cs
+++ Equipment-Maintenance-in-VR/Assets/ScreenUIController.cs
@@ -11,41 +11,63 @@ public class ScreenUIController : MonoBehaviour {
     public float textDist = .75f;
     public float verticalDist = 3.0f;
     public float horizontalDist = 0.0f;
-    
+	public TextMesh text3D;
+	public bool testing3D = false;
+
 	// Use this for initialization
 	void Start () {
         
         screenCanvas = GameObject.Find("ScreenCanvas").GetComponent<Canvas>();
         topText = GameObject.Find("TopText").GetComponent<Text>();
-    }
+		if (testing3D)
+			topText.gameObject.SetActive(false);
+	}
 	
 	// Update is called once per frame
 	void Update () {
- 
-        screenCanvas.transform.position =  trackingObject.transform.position + trackingObject.transform.forward * textDist + trackingObject.transform.up * -(verticalDist) + trackingObject.transform.right * horizontalDist ;
-        screenCanvas.transform.rotation = trackingObject.transform.rotation;
+		if (!testing3D)
+		{
+			screenCanvas.transform.position = trackingObject.transform.position + trackingObject.transform.forward * textDist + trackingObject.transform.up * -(verticalDist) + trackingObject.transform.right * horizontalDist ;
+			screenCanvas.transform.rotation = trackingObject.transform.rotation;
+		}
     }
 
     public void SetTopText(string text)
     {
-        topText.text = text;
-        ShowTopText();
+		if (testing3D)
+		{
+			text3D.text = text;
+			text3D.transform.position = trackingObject.transform.position + trackingObject.transform.forward * textDist + trackingObject.transform.up * -(verticalDist) + trackingObject.transform.right * horizontalDist;
+			text3D.transform.rotation = trackingObject.transform.rotation;
+		}
+		else
+			topText.text = text;
+		ShowTopText();
     }
 
     public void ClearTopText()
     {
-        topText.text = "";
-        HideTopText();
+		if (testing3D)
+			text3D.text = "";
+		else
+			topText.text = "";
+		HideTopText();
     }
 
     public void ShowTopText()
     {
-        topText.gameObject.SetActive(true);
-    }
+		if (testing3D)
+			text3D.gameObject.SetActive(true);
+		else
+			topText.gameObject.SetActive(true);	
+	}
 
     public void HideTopText()
     {
-        topText.gameObject.SetActive(false);
-    }
+		if (testing3D)
+			text3D.gameObject.SetActive(false);
+		else
+			topText.gameObject.SetActive(false);
+	}
 
 }
