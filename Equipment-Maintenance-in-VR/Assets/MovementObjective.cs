0b22dc537eb3c1357fe39d37dd6b088ae52cc71b
dthigpen <davidthigs@gmail.com>
2018-11-08 16:57:13 -0600

    Small changes

--- Equipment-Maintenance-in-VR/Assets/MovementObjective.cs
+++ Equipment-Maintenance-in-VR/Assets/MovementObjective.cs
@@ -10,13 +10,14 @@ public class MovementObjective : MonoBehaviour, ObjectiveCommands {
     public TeleportPoint teleportPoint;
     public Hand hand;
     public SteamVR_Action_Boolean teleportAction;
-    public int frameSkip = 15;
     public float distance = 0.2f;
     private int frameCount = 0;
 
-    private bool playerDetected = false;
     private bool teleportActiveState = false;
     private ObjectiveSubject objectiveSubject;
+
+    // TODO Find out why distance between vrCamera and teleportPoint is > 1m 
+    // TODO Optionally disable teleportArea that covers entire ground to clearly indicate where they can go
 
     public void OnObjectiveReset()
     {
@@ -43,8 +44,6 @@ public class MovementObjective : MonoBehaviour, ObjectiveCommands {
 
         if (Vector3.Distance(tempTransform1, tempTransform2) <= distance)
         {
-            Debug.Log("Player detected on teleport point");
-            playerDetected = true;
             GetComponent<ObjectiveSubject>().NotifyCompletion();
             teleportAction.RemoveOnChangeListener(OnTeleportActionChange, hand.handType);
         }
@@ -64,28 +63,5 @@ public class MovementObjective : MonoBehaviour, ObjectiveCommands {
         ObjectiveSubject objectiveSubject = GetComponent<ObjectiveSubject>();
         teleportPoint.gameObject.SetActive(teleportActiveState);
         
-    }
-    
-    void Update()
-    {
-        // TODO move this from the update to an event, triggering everytime the user releases the teleport buttton
-        //if (GetComponent<ObjectiveSubject>() != null && GetComponent<ObjectiveSubject>().objectiveState == Objective.ObjectiveStates.InProgress)
-        //{
-        //    if (!playerDetected && frameCount++ % frameSkip == 0)
-        //    {
-        //        frameCount = 1;
-        //        var tempTransform1 = vrCamera.transform.position;
-        //        tempTransform1.y = 0;
-        //        var tempTransform2 = teleportPoint.transform.position;
-        //        tempTransform2.y = 0;
-
-        //        if (Vector3.Distance(tempTransform1, tempTransform2) <= distance)
-        //        {
-        //            Debug.Log("Player detected on teleport point");
-        //            playerDetected = true;
-        //            GetComponent<ObjectiveSubject>().NotifyCompletion();
-        //        }
-        //    }
-        //}
     }
 }
