c926959cdf8b84d42f717d9e9641c1ee712c1d41
dthigpen <davidthigs@gmail.com>
2018-11-15 15:17:03 -0600

    Made audio queue for hints

---
+++ Equipment-Maintenance-in-VR/Assets/AudioQueue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioQueue : MonoBehaviour {
+
+    public AudioSource audioSource;
+    private Queue<AudioClip> audioClipsQueue;
+    private bool isPaused = false;
+    
+    // Use this for initialization
+    void Start () {
+        audioClipsQueue = new Queue<AudioClip>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(audioSource != null && audioClipsQueue.Count > 0 && !isPaused && !audioSource.isPlaying)
+        {
+            audioSource.clip = audioClipsQueue.Dequeue();
+            audioSource.Play();
+        }
+	}
+
+    public void AddAudioClipToQueue(AudioClip audioClip)
+    {
+        audioClipsQueue.Enqueue(audioClip);
+    }
+
+    public void PlayOneShot(AudioClip audioClip)
+    {
+        if (audioSource != null)
+            audioSource.PlayOneShot(audioClip);
+    }
+
+    public void PauseQueue()
+    {
+        isPaused = true;
+        audioSource.Pause();
+    }
+
+    public void UnPauseQueue()
+    {
+        isPaused = false;
+        audioSource.UnPause();
+    }
+}
