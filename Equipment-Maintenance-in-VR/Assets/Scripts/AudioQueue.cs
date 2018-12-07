57952f18c47ce2b8f60dadfe2d2cdf46d15f1bfa
dthigpen <davidthigs@gmail.com>
2018-12-07 12:24:23 -0600

    Fix audio source change and skid mount collider

--- Equipment-Maintenance-in-VR/Assets/Scripts/AudioQueue.cs
+++ Equipment-Maintenance-in-VR/Assets/Scripts/AudioQueue.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class AudioQueue : MonoBehaviour
 {
     public AudioSource audioSource;
+    private AudioSource nextAudioSource;
     public bool muteOnStart = true;
     private Queue<AudioClip> audioClipsQueue = new Queue<AudioClip>();
     private bool isPaused = false;
+    private bool waitingToChangeSource = false;
 
-    
     // Use this for initialization
     void Start ()
     {
@@ -20,10 +21,12 @@ public class AudioQueue : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		if(audioSource != null && audioClipsQueue.Count > 0 && !isPaused && !audioSource.isPlaying)
+		if(!waitingToChangeSource && audioSource != null && audioClipsQueue.Count > 0 && !isPaused && !audioSource.isPlaying)
         {
-            audioSource.clip = audioClipsQueue.Dequeue();
-            audioSource.Play();
+            
+                audioSource.clip = audioClipsQueue.Dequeue();
+                audioSource.Play();
+            
         }
 	}
 
@@ -35,14 +38,27 @@ public class AudioQueue : MonoBehaviour
     public void AddAudioClipToQueue(AudioClip audioClip, bool stopCurrentAudio = false)
     {
         audioClipsQueue.Enqueue(audioClip);
+        if (audioSource != null && audioClipsQueue.Count > 0 && !isPaused && !audioSource.isPlaying)
+        {
+            audioSource.clip = audioClipsQueue.Dequeue();
+            audioSource.Play();
+        }
     }
-
+    
     public void ChangeAudioSource(AudioSource newAudioSource)
     {
-        if (audioSource.isPlaying)
-            audioSource.Stop();
+        StartCoroutine(WaitToChangeSource(newAudioSource));
+    }
+
+    private IEnumerator WaitToChangeSource(AudioSource newAudioSource)
+    {
+        yield return new WaitWhile(() => !audioSource.isPlaying);
+        waitingToChangeSource = true;
+
+        yield return new WaitWhile(() => audioSource.isPlaying);
+
         audioSource = newAudioSource;
-        audioSource.Play();
+        waitingToChangeSource = false;
     }
 
     public void PlayOneShot(AudioClip audioClip, float volume = 1.0f)
